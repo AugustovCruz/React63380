03eeba8d50f21f184ce55075dcda5c8d0a72fd5a
AugustovCruz <augusto.tito.96@gmail.com>
2024-12-08 12:37:05 -0300

    update route

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router";
 import { CartWidget } from "../CartWidget/CartWidget";
-import "../Nav/navbar.css"
+import "../Nav/Navbar.css"
 
 function Nav() {
     return (
